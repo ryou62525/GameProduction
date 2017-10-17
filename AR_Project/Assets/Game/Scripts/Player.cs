e6d131df31afe63910b8867ddaae70ca05d0d669
ryou62525 <majaj62525@icloud.com>
2017-10-17 14:15:22 +0900

    add GravityTest Scene

---
+++ AR_Project/Assets/Game/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 10.0f;
+
+    [SerializeField]
+    private Text text = null;
+
+    void Update()
+    {
+        var dir = Vector3.zero;
+        dir.x = Input.acceleration.x;
+        dir.y = Input.acceleration.y;
+
+        text.GetComponent<Text>().text = dir.ToString();
+
+        if (dir.sqrMagnitude > 1)
+        {
+            dir.Normalize();
+        }
+
+        dir *= Time.deltaTime;
+
+        transform.Translate(dir * speed);
+    }
+}
