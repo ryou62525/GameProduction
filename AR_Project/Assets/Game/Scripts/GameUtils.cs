26bd963365aaf285def31f114decfdd83b008598
ryou62525 <majaj62525@icloud.com>
2017-10-20 16:23:07 +0900

    機能分けをしました

---
+++ AR_Project/Assets/Game/Scripts/GameUtils.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUtils : MonoBehaviour
+{
+	/// <summary>
+	/// Ises the target angle y.
+	/// 現在のY軸の角度が目標の2点の角度の範囲内かどうか調べる
+	/// </summary>
+	/// <returns><c>true</c>, if target angle y was ised, <c>false</c> otherwise.</returns>
+	/// <param name="targetEulerAngleY">Target euler angle y.</param>
+	/// <param name="thisTransform">This transform.</param>
+	public static bool IsTargetAngleY(float targetEulerAngleY, Transform thisTransform, float diff)
+	{
+		var from = targetEulerAngleY - diff;
+		var to = targetEulerAngleY + diff;
+		if (from <= thisTransform.localEulerAngles.y && to >= thisTransform.localEulerAngles.y) return true;
+		return false;
+	}
+}
